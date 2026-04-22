3e7c88f75c964c8b3171e1c9f78231b4d7775bc1
Jonas Vetsch <jonas@vetsch.com>
2026-04-22 20:51:29 +0200

    Add more projects, improve copywriting, fix bug where ressources were not found due to path issues.

--- script/index.js
+++ script/index.js
@@ -1,8 +1,16 @@
 // Load Footer
 async function loadFooter() {
-    const response = await fetch('/html/footer.html');
+    const response = await fetch('html/footer.html');
     const footerHTML = await response.text();
     document.getElementById('footer-placeholder').innerHTML = footerHTML;
 }
 
+// Load Footer
+async function loadHeader() {
+    const response = await fetch('html/header.html');
+    const footerHTML = await response.text();
+    document.getElementById('header-placeholder').innerHTML = footerHTML;
+}
+
+loadHeader();
 loadFooter();
